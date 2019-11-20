144cbd7ad7dcdfd7c928e553883e5a886ea3c4ee
MSP Enterprise <mspenterprise@MacBook-Pro-MSP-3.local>
2019-11-20 17:39:43 +0700

    app

---
+++ js/global.js
@@ -0,0 +1,6 @@
+// var baseURL = 'https://gateandway.herokuapp.com/'
+// var baseURLWEB = 'http://learn.gateandway.com/'
+
+var baseURL = 'http://localhost:8080/'
+var baseURLWEB = 'http://localhost/gateandway/'
+
